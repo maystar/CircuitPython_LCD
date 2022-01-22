96c3c9f3a5b80b6f25deb83d5d5e7dcc7ebad41e
Fabian May <fabian.may@wps.de>
2022-01-22 15:27:52 +0100

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='I2C_HD77480_LCD',
-    version='0.1.0',
-    packages=['lcd'],
-    url='',
-    license='MIT',
-    author='Fabian',
-    author_email='maystar@web.de',
-    description='LCD driver for HD4480 behind PCF8574',
-    install_requires=[
-        'Adafruit-Blinka',
-        'adafruit-circuitpython-busdevice'
-    ]
-)
+setup()
